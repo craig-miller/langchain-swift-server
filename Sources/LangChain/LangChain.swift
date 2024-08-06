c6638125f4a3ef0ec902071f160ea97a426b6ad9
Craig Miller <craig@nextidea.io>
2024-08-06 14:02:50 -0700

    Remove Common Crypto dependency.  Replace with CryptoSwift

--- Sources/LangChain/LangChain.swift
+++ Sources/LangChain/LangChain.swift
@@ -1,50 +1,48 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by 顾艳华 on 2023/6/11.
 //
 
 import Foundation
-//import CommonCrypto
 import CryptoSwift
 
 enum LangChainError: Error {
-    case LoaderError(String)
-    case ChainError
-    case ToolError
+	case LoaderError(String)
+	case ChainError
+	case ToolError
 }
 
 extension Data{
-    public func sha256() -> String{
-        return hexStringFromData(input: digest(input: self as NSData))
-    }
-    
-    private func digest(input : NSData) -> NSData {
-        let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
-        var hash = [UInt8](repeating: 0, count: digestLength)
-        CC_SHA256(input.bytes, UInt32(input.length), &hash)
-        return NSData(bytes: hash, length: digestLength)
-    }
-    
-    private  func hexStringFromData(input: NSData) -> String {
-        var bytes = [UInt8](repeating: 0, count: input.length)
-        input.getBytes(&bytes, length: input.length)
-        
-        var hexString = ""
-        for byte in bytes {
-            hexString += String(format:"%02x", UInt8(byte))
-        }
-        
-        return hexString
-    }
-}
+	public func sha256() -> String{
+		return hexStringFromData(input: digest(input: self as NSData))
+	}
 
-public extension String {
-    func sha256() -> String{
-        if let stringData = self.data(using: String.Encoding.utf8) {
-            return stringData.sha256()
-        }
-        return ""
-    }
-}
+	private func digest(input : NSData) -> NSData {
+		let data = [UInt8](input)
+		var hash: [UInt8] = SHA3(variant: .sha256).calculate(for: data)
+		return NSData(bytes: hash, length: hash.count )
+	}
+
+	private  func hexStringFromData(input: NSData) -> String {
+				var bytes = [UInt8](repeating: 0, count: input.length)
+				input.getBytes(&bytes, length: input.length)
+
+				var hexString = ""
+				for byte in bytes {
+					hexString += String(format:"%02x", UInt8(byte))
+				}
+
+				return hexString
+			}
+	}
+
+	public extension String {
+				func sha256() -> String{
+					if let stringData = self.data(using: String.Encoding.utf8) {
+						return stringData.sha256()
+					}
+					return ""
+				}
+			}
--- Sources/LangChain/utilities/jwt/Cryptor.swift
+++ Sources/LangChain/utilities/jwt/Cryptor.swift
@@ -1,59 +1,54 @@
 import Foundation
-import CommonCrypto
+//import CommonCrypto
 //import Crypto
+import CryptoSwift
 
 class Cryptor {
-    
-    static func hmac(string: String, algorithm: Algorithm, key: String) -> String? {
-        guard let key = key.data(using: .utf8) else { return nil }
-        guard let string = string.data(using: .utf8) else { return nil }
-        
-        let context = UnsafeMutablePointer<CCHmacContext>.allocate(capacity: 1)
-        defer { context.deallocate() }
-        
-        key.withUnsafeBytes { (buffer: UnsafePointer<UInt8>) in
-            CCHmacInit(context, algorithm.HMACAlgorithm, buffer, size_t(key.count))
-        }
 
-        string.withUnsafeBytes { (buffer: UnsafePointer<UInt8>) in
-          CCHmacUpdate(context, buffer, size_t(string.count))
-        }
+	static func hmac(string: String, algorithm: Algorithm, key: String) -> String? {
+		//		        guard let key = key.data(using: .utf8) else { return nil }
+		//		        guard let string = string.data(using: .utf8) else { return nil }
+		//
+		//		        let context = UnsafeMutablePointer<CCHmacContext>.allocate(capacity: 1)
+		//		        defer { context.deallocate() }
+		//
+		//		        key.withUnsafeBytes { (buffer: UnsafePointer<UInt8>) in
+		//		            CCHmacInit(context, algorithm.HMACAlgorithm, buffer, size_t(key.count))
+		//		        }
+		//
+		//		        string.withUnsafeBytes { (buffer: UnsafePointer<UInt8>) in
+		//		          CCHmacUpdate(context, buffer, size_t(string.count))
+		//		        }
 
-        var hmac = Array<UInt8>(repeating: 0, count: Int(algorithm.digestLength))
-        CCHmacFinal(context, &hmac)
+		//		        var hmac = Array<UInt8>(repeating: 0, count: Int(algorithm.digestLength))
+		//		        CCHmacFinal(context, &hmac)
 
-        return Data(hmac).base64URLEncodedString()
-    }
+		do {
+			let key: Array<UInt8> = try Array<UInt8>(hex: key)
+			let msg: Array<UInt8> = try Array<UInt8>(hex:string)
+			let hmac: Array<UInt8> = try HMAC(key: key, variant: algorithm.HMACAlgorithm).authenticate(msg)
+			return Data(hmac).base64URLEncodedString()
+		} catch {
+		}
+		return nil
+	}
 
-    enum Algorithm {
-        case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+	enum Algorithm {
+		case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
 
-        var HMACAlgorithm: CCHmacAlgorithm {
-            var result: Int = 0
-            switch self {
-            case .MD5:      result = kCCHmacAlgMD5
-            case .SHA1:     result = kCCHmacAlgSHA1
-            case .SHA224:   result = kCCHmacAlgSHA224
-            case .SHA256:   result = kCCHmacAlgSHA256
-            case .SHA384:   result = kCCHmacAlgSHA384
-            case .SHA512:   result = kCCHmacAlgSHA512
-            }
-            return CCHmacAlgorithm(result)
-        }
-
-        var digestLength: Int {
-            var result: Int32 = 0
-            switch self {
-            case .MD5:      result = CC_MD5_DIGEST_LENGTH
-            case .SHA1:     result = CC_SHA1_DIGEST_LENGTH
-            case .SHA224:   result = CC_SHA224_DIGEST_LENGTH
-            case .SHA256:   result = CC_SHA256_DIGEST_LENGTH
-            case .SHA384:   result = CC_SHA384_DIGEST_LENGTH
-            case .SHA512:   result = CC_SHA512_DIGEST_LENGTH
-            }
-            return Int(result)
-        }
-    }
+		var HMACAlgorithm: HMAC.Variant {
+			var result: HMAC.Variant = .md5
+			switch self {
+				case .MD5:      result = .md5
+				case .SHA1:     result = .sha1
+				case .SHA224:   result = .sha2(.sha224)
+				case .SHA256:   result = .sha2(.sha256)
+				case .SHA384:   result = .sha2(.sha384)
+				case .SHA512:   result = .sha2(.sha512)
+			}
+			return result
+		}
+	}
 }
 
 
